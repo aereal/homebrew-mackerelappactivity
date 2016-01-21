6d061cf249cb358a81c2b2d08b7792b61c05eb12
aereal <aereal@aereal.org>
2016-01-21 22:42:44 +0900

    + mackerelappactivity

---
+++ Casks/mackerelappactivity.rb
@@ -0,0 +1,11 @@
+cask 'mackerelappactivity' do
+  version '1.2'
+  sha256 '4474abb0927e76db05f478738889d6725063dc419c066babfe22050f3903deb2'
+
+  url 'https://github.com/pokutuna/MackerelAppActivity.app/raw/master/releases/MackerelAppActivity-1.2.app.zip'
+  name 'MackerelAppActivity'
+  homepage 'https://github.com/pokutuna/MackerelAppActivity.app'
+  license :oss
+
+  app 'MackerelAppActivity.app'
+end
